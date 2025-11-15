d1480ce7740ce848ef054bf0e3358f39fd3eeddf
Xuan Manh 9999 <nguyenxuanmanh030929@gmail.com>
2025-11-15 20:33:10 +0700

    Update

--- server.js
+++ server.js
@@ -335,7 +335,7 @@ app.post("/api/system-status", (req, res) => {
 
 // Start server
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on http://g:${PORT}`);
 });
 
 // Graceful shutdown
